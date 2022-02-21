22cf411def8f2099fb5fc68d49a9c4f3a0ac63bd
ayslaw <ayoubf.chp@gmail.com>
2022-02-20 19:35:27 -0500

    new tweets by user appear on page without refreshing + validations

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,76 +4,88 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-
 $(document).ready(function () {
-
-
-
-  const data = [
-   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-  
-    "content": {
-     "text": "If I have seen further it is by standing on the shoulders of giants"
-   },
-   "created_at": 1461116232227
-  },
-  {
-   "user": {
-     "name": "Descartes",
-     "avatars": "https://i.imgur.com/nlhLi3I.png",
-     "handle": "@rd" },
-   "content": {
-     "text": "Je pense , donc je suis"
-   },
-   "created_at": 1461113959088
-  }
-  ];
-  
-  const createTweetElement = function(tweet) {
-   let date = timeago.format(tweet.created_at);
-     let $tweet = $(
-   `<article class="tweet-header wrapper">
-         <header>
-               <img class="left" id="pic" src="${tweet.user.avatars}">
-               <h1 class="left">${tweet.user.name}</h1>
-             <h5 class="right">${tweet.user.handle}</h5>
-         </header>
-         <p class="message">${tweet.content.text}</p>
-         <footer class="tweet-footer">
-           <div>
-           <p class="left"> ${date}</p>
-           </div>
-           <div>
-             <i class="fas fa-retweet"></i>
-             <i class="fas fa-heart"></i>
-             <i class="fas fa-flag"></i>
-           </div>
-         </footer>
-         </article> `)
-         console.log("testing", $tweet);
-         return $tweet;
-       };
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
       
-      
-       const renderTweets = function(tweets) {
-           $("#tweets").empty();
-           console.log($('.tweets-container'));
-           for (let tweet of tweets) {
-            $('.tweets-container').prepend(createTweetElement(tweet));
-            console.log(tweets);
-           }
-  }
-  renderTweets(data);
+    }).then((response) => {
+      // console.log("RESPONSE: ");
+      // console.log(response);
+      renderTweets(response);
+      $('#tweet-text').val("");
+      $(".counter").text(140);
+    });
+  };
+  loadTweets();
   
-  const $newTweet = $('#submit-tweet');
-     $newTweet.on('submit', function(event) {
-       event.preventDefault();
-     });
-  
-  }); 
+ 
+ 
+ const createTweetElement = function(tweet) {
+  let date = timeago.format(tweet.created_at);
+  let $tweet = $(
+  `<article class="tweet-header wrapper">
+        <header>
+              <img class="left" id="pic" src="${tweet.user.avatars}">
+              <h1 class="left">${tweet.user.name}</h1>
+            <h5 class="right">${tweet.user.handle}</h5>
+          </header>
+          <p class="message">${tweet.content.text}</p>
+          <footer class="tweet-footer">
+            <div>
+              <p class="left"> ${date}</p>
+            </div>
+            <div>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+              <i class="fas fa-flag"></i>
+            </div>
+          </footer>
+        </article> `)
+ 
+  return $tweet;
+ };
+ 
+ 
+ const renderTweets = function(tweets) {
+    $("#tweets").empty();
+    console.log($('.tweets-container'));
+    for (let tweet of tweets) {
+      $('.tweets-container').prepend(createTweetElement(tweet));
+    }
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container 
+ }
+ 
+ 
+ 
+ const $newTweet = $('#tweet-form');
+  $newTweet.on('submit', function(event) {
+    event.preventDefault();
+    const tweet = $("#tweet-text").val().length;
+    console.log(tweet);
+    if (!tweet) {
+      $('#errorMessage').show();
+      $('#errorMessage').text("Tweet cannot be empty!");
+    };
+    if (tweet > 140) {
+      $('#errorMessage').show();
+      $('#errorMessage').text("Tweet can't be longer than 140 characters!");
+    } else {
+      const val = $(this).serialize();
+      $.ajax("/tweets", {
+        method: "POST",
+        data: val,
+      })
+        .then(() => {
+          $('#errorMessage').hide();
+          loadTweets();
+          $("#tweet-text").val("");
+       
+        });
+    }
+  })
+ 
+ });
